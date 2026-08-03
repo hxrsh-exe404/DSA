a0f4d3542c620f4dc33c76203b74db0044acc1eb
hxrsh-exe404 <harshkumarhk1930@gmail.com>
2026-08-03 13:02:26 +0530

    Leetcode_26

---
+++ DAY 056/Leetcode_26.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+}
+
+int main() {
+    vector<int> nums = {1, 1, 2, 2, 3, 4, 4};
+
+    int k = removeDuplicates(nums);
+
+    cout << "Number of unique elements: " << k << endl;
+
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
