250806aed4616835996c09d377a1606a2c749472
hxrsh-exe404 <harshkumarhk1930@gmail.com>
2026-08-13 23:44:10 +0530

    Leetcode 118

---
+++ DAY 066/Leetcode_118.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < numRows; i++) {
+        vector<int> row(i + 1, 1);
+
+        for (int j = 1; j < i; j++) {
+            row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+        }
+
+        ans.push_back(row);
+    }
+
+    return ans;
+}
+
+int main() {
+    int numRows = 5;
+
+    vector<vector<int>> result = generate(numRows);
+
+    for (auto row : result) {
+        for (int x : row) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
